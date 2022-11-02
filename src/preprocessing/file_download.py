2ce0bfae0603c66d5d89c6f2a84ddbb819099f78
anosillus <anosillus@gmail.com>
2022-11-02 09:38:56 +0900

    write logic of file_downlader

--- src/preprocessing/file_download.py
+++ src/preprocessing/file_download.py
@@ -1,73 +1,71 @@
-"""set up file url list for downlaod xls files."""
-import datetime
-import hashlib
-import json
+"""set up file url list for download xls files."""
 import logging
 import operator
-import os
 import re
-from abc import ABCMeta
-from abc import abstractmethod
 from pathlib import Path
 from re import Pattern
-from typing import Any
-from typing import Final
-from typing import Optional
-from urllib.parse import urljoin
-from zoneinfo import ZoneInfo
 
 import requests
 import structlog
-from bs4 import BeautifulSoup
 from pydantic import FileUrl
-from pydantic import HttpUrl
 from requests import RequestException
-from requests import Response
 from structlog import BoundLogger
-from structlog.stdlib import BoundLogger
 from tqdm.auto import tqdm
 
-from .data_path import RAW_DATA_DIR
 from .data_path import ROOT_DIR
-from .data_type import EventInfo
 from .util import default_data_dir
+from .util import DownloadLog
 from .util import read_json_file
+from .util import sha256sum
 from .util import time_for_record
 from .util import today
 from .util import write_json_file
 
 
 class FileDownloader:
-    def __init__(self, urls_dict_path: Path ):
+    def __init__(self, urls_dict_path: Path):
         self.urls_dict_path = urls_dict_path
-        self.urls:lsit[FileUrl] = []
-        self.file_place =
+        self.file_place: Path = default_data_dir()
         self.logger: BoundLogger = structlog.stdlib.get_logger()
-        self.res: Response
-        self.url_list: list[FileUrl]
-        # self.data_dict: dict[str, str | list[FileUrl]]
-
-    def run(self):
-        self.dowload_files()
-
-    def target_urls(self):
-        urls_json_data = read_json_data(self.urls_dict_path)
-
-        self.urls = urls_json_data.get("urls")
-        self.logger.info("read json data",
-                json_path=self.urls_dict_path,
-                file_saved_date=urls_json_data.get("date"),
-                urls_amount = len(self.urls))
-
-
-
-
-
-
+        self.urls: list[FileUrl] = []
+        self.download_logs: list[DownloadLog] = []
+        self.download_log_path = Path(
+            default_data_dir() / "download_log_" + today() + ".json"
+        )
 
     @staticmethod
-    def _download_file(url: FileUrl, file_path:Path):
-        res = requests.get(url, stream=True, timeout=10)
+    def _remove_catalog_file_name_noise(name: str) -> str:
+        """This function remove the noise in name of okinawa statistics data files."""
+        braket_pattern: Pattern[str] = r"\(.*?\)"
+        # re.compile is faster, but I don't use that for clean global scoope.
+        # this braket_pattern transform 2010(1).xls into 2010.xls
+
+        return (
+            re.sub(pattern=braket_pattern, repl="", string=name)
+            .replace(" ", "")
+            .replace("r", "")
+            .replace("_1", "")
+            .replace("pop", "")
+        )
+
+    def run(self) -> None:
+        self.read_target_urls()
+        self.download_files()
+
+    def read_target_urls(self) -> None:
+        urls_json_data = read_json_file(self.urls_dict_path)
+
+        self.urls = urls_json_data.get("urls")
+        self.logger.info(
+            "result of json data read",
+            json_path=self.urls_dict_path,
+            file_downloaded_date=urls_json_data.get("date"),
+            urls_amount=len(self.urls),
+        )
+
+    @staticmethod
+    def _download_file(file_url: FileUrl, file_path: Path) -> None:
+        res = requests.get(file_url, stream=True, timeout=10)
         with tqdm.wrapattr(
             open(file_path, "wb"),
             "write",
@@ -95,18 +93,40 @@ class FileDownloader:
                     file_url=file_url,
                     file_path=Path(self.file_place / file_name),
                 )
-                self.download_event_log.append(
-                    self._shape_event_log(name=file_name, url=file_url)
-                )
+                self.download_logs.append(self._shape_log(name=file_name, url=file_url))
 
-        except Exception as err:
+        except RequestException as err:
             self.logger.info(
                 "Stopped at files downloading.",
                 trouble_file=file_name,
                 file_place=self.file_place,
                 message=err,
-                download_event_log=self.download_event_log,
+                download_logs=self.download_logs,
             )
 
         finally:
-            self.save_log()
+            self._write_log()
+
+    def _write_log(self):
+        data_dicts: list[dict[str, str]] = [
+            info._asdict() for info in self.download_log
+        ]
+        data_dicts = sorted(data_dicts, key=operator.itemgetter("name"))
+
+        write_json_file(
+            json_path=self.download_log_path, data_dict=data_dicts, logger=self.logger
+        )
+
+    def _shape_log(self, name, url):
+        try:
+            path = str(self.file_path.relative_to(ROOT_DIR))
+        except ValueError:
+            path = str(self.file_path)
+
+        return DownloadLog(
+            name=self._remove_catalog_file_name_noise(name),
+            url=url,
+            path=path,
+            hash_value=sha256sum(path),
+            download_date=time_for_record(),
+        )
