74ddaca29e2ce0ed9d96c202c66a73d13886fa0f
anosillus <anosillus@gmail.com>
2022-11-04 02:38:42 +0900

    remove raw data

--- old_tests/make_mock_data.py
+++
@@ -1,9 +0,0 @@
-import requests
-import wget
-
-response = requests.get("https://www.pref.okinawa.jp/toukeika/estimates/estidata.html")
-with open("./mock_data/response_mock_at_2022_10_25.txt", "w") as f:
-    f.write(response.text)
-
-file_url = "https://www.pref.okinawa.jp/toukeika/estimates/2022/pop202209.xls"
-wget.download(file_url, out="./mock_data")
